c22f4532ba977bdb30e3652f45d488f8d7fe6b9a
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-02 06:39:34 -0700

    Update index.php

--- index.php
+++ index.php
@@ -356,7 +356,9 @@ if(strpos($mystring3, $word6) !== false) {
           
              
          if(count($str_arr) >= 6) {
-             if(count($result) >= count($str_arr)-3) {   print_r($result2);  }
+             if(count($result) >= count($str_arr)-2) {   print_r($result2);  }
+            else if($duplicates >= 1 && count($result) >= 4) { print_r($result2); }
+            else if($duplicates >= 2 && count($result) >= 3) { print_r($result2);   }
          }    
          else if(count($str_arr) == 5) {
              if(count($result) >= count($str_arr)-1) {   print_r($result2);  }
