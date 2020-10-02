127eb3567e6bf2e75d4c5426e5ba72945fab5dec
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-02 06:20:56 -0700

    Update index.php

--- index.php
+++ index.php
@@ -365,12 +365,11 @@ if(strpos($mystring3, $word6) !== false) {
          else if(count($str_arr) == 3 || count($str_arr) == 4)
          {
           echo $duplicates;
-             if(count($result) >= count($str_arr)-1) {   print_r($result2);  } 
+             if(count($result) == count($str_arr)) {   print_r($result2);  } 
             // else if($color_counts1 >= 3 && count($result) >= 2) { print_r($result2); }
            //  else if($size_counts1 >= 3 && count($result) >= 2) { print_r($result2); }
             // else if($material_counts1 >= 3 && count($result) >= 2) { print_r($result2); }
-            // else if($color_counts1 >= 1 && $material_counts1 >= 1 && count($result) >= 3) { print_r($result2); }
-          
+            // else if($color_counts1 >= 1 && $material_counts1 >= 1 && count($result) >= 3) { print_r($result2); }         
              else if($duplicates >= 1 && count($result) >= 2) { print_r($result2);   }
          } else if(count($str_arr) == count($result)) {
              print_r($result2); 
