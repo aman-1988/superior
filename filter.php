e12e983db2c5dea44da5f4f2da2a4ef5211210e0
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-11 19:29:11 +0530

    Update filter.php

--- filter.php
+++ filter.php
@@ -142,7 +142,7 @@ $productss = getorder("https://".$SHOPIFY_SHOP."/admin/api/2020-07/products.json
        $price1 = '<div class="prod-price"><span class="normal-money">Price $'.$price_varient1.'</span></div>';
       }
       
-      if( $price_varient1 > 100000 && $product_type1 == 'Equipment') {       
+      if( $price_varient1 > 1000 && $product_type1 == 'Equipment') {       
        $price2 = '<span class="Retail">Retail Price</span><span class="money"> $'.$price_varient1.'</span><br><button style="text-color: #FFFFFF;"><span class="email"><a style="color: #FFFFFF;" href="'.$collpath.'/products/'.$handle1.'">Email Me My Price</span></a></button>';
       }
       
