aee675d13c77b88f7b98db4fa9f49c2c51a272bf
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-15 01:41:38 -0700

    Update filter.php

--- filter.php
+++ filter.php
@@ -416,11 +416,11 @@ if(strpos($mystring3, $word6) !== false) {
        else if(!empty($str_arr)) {
              
         
-        print_r($str_arr2);
-        echo "<br>";
+        //print_r($str_arr2);
+        //echo "<br>";
         
-        print_r($str_arr);
-        echo "<br><br>";
+        //print_r($str_arr);
+        //echo "<br><br>";
         
         
          $result=array_intersect($str_arr2,$str_arr);
