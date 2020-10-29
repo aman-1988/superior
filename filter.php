0e99014079fcce54ccd8c3f3f752ee116de4cbfa
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-29 19:28:40 +0530

    Update filter.php

--- filter.php
+++ filter.php
@@ -235,8 +235,9 @@ $duplicates = count($str_arrs_1) - count($unique_colors);
         // $price_varient1 = $product_line_items3['variants'][0]['price'];
          //$price_compare_at_price = $product_line_items3['variants'][0]['compare_at_price'];
         
-      $queries3 = array('query' => 'query { product(id: "'.$admin_graphql_api_id.'") { variants(first:1) { edges { node { compareAtPrice  price } } }    } }');
-    $productss2 = httppost("https://".$SHOPIFY_SHOP."/admin/api/unstable/graphql.json",$queries3);
+     // $queries3 = array('query' => 'query { product(id: "'.$admin_graphql_api_id.'") { variants(first:1) { edges { node { compareAtPrice  price } } }    } }');
+   // $productss2 = httppost("https://".$SHOPIFY_SHOP."/admin/api/unstable/graphql.json",$queries3);
+       $productss2 = getorder("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");
       
        //$price_varient1_1 = $productss2['data']['product']['variants']['edges'][0]['node']['price'];
        //$price_compare_at_price_1 = $productss2['data']['product']['variants']['edges'][0]['node']['compareAtPrice'];
