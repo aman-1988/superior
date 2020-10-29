286a4d6ee75dd3e22421ab5ac37ae18ce927a97b
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-29 19:17:06 +0530

    Update filter.php

--- filter.php
+++ filter.php
@@ -235,11 +235,11 @@ $duplicates = count($str_arrs_1) - count($unique_colors);
         // $price_varient1 = $product_line_items3['variants'][0]['price'];
          //$price_compare_at_price = $product_line_items3['variants'][0]['compare_at_price'];
         
-      $queries3 = array('query' => 'query { product(id: "'.$admin_graphql_api_id.'") { variants(first:1) { edges { node { compareAtPrice  price } } }    } }');
+    //  $queries3 = array('query' => 'query { product(id: "'.$admin_graphql_api_id.'") { variants(first:1) { edges { node { compareAtPrice  price } } }    } }');
    // $productss2 = httpPost9("https://".$SHOPIFY_SHOP."/admin/api/unstable/graphql.json",$queries3);
       
-       $price_varient1_1 = $productss2['data']['product']['variants']['edges'][0]['node']['price'];
-       $price_compare_at_price_1 = $productss2['data']['product']['variants']['edges'][0]['node']['compareAtPrice'];
+       //$price_varient1_1 = $productss2['data']['product']['variants']['edges'][0]['node']['price'];
+       //$price_compare_at_price_1 = $productss2['data']['product']['variants']['edges'][0]['node']['compareAtPrice'];
       //echo $price_varient1_1." - ".$price_compare_at_price_1;
       
        $price_varient1 = $product_line_items[$keys3]['variants'][0]['price'];
