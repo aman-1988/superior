47a365eb865bbe7585b7d1407729e999990e2616
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-30 15:31:36 +0530

    Update test_filter.php

--- test_filter.php
+++ test_filter.php
@@ -422,6 +422,7 @@ if(strpos($mystring3, $word6) !== false) {
          
           //$result2 = $handle1. " - $".$price1." - ".$proimgs1;
       
+      if(!empty($price_varient1)) {
       $result2 = '
 <div data-tag="'.htmlspecialchars($allproductatgs).'" class="'.htmlspecialchars($allproductatgs).' product-index desktop-3 tablet-2 mobile-half" data-alpha="" data-price="'.$price_varient1.'" style="height:620px;">     
 <div class="prod-border"><div class="prod-container">
@@ -445,7 +446,7 @@ if(strpos($mystring3, $word6) !== false) {
 <a style="text-align:center;color:#003870; font-weight:bold; font-size:16px;" href="'.$collpath.'/products/'.$handle1.'"><p>'.htmlspecialchars($protitle2).'</p></a></div>
 </div>
 </div>';
-      
+      }
 
          
       if(empty($varientss)) {
