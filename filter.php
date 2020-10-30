9c590482433c87ada4a2067a48e2f271e638108c
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-30 15:58:08 +0530

    Update filter.php

--- filter.php
+++ filter.php
@@ -440,7 +440,7 @@ if(strpos($mystring3, $word6) !== false) {
       
          
           //$result2 = $handle1. " - $".$price1." - ".$proimgs1;
-      if(!empty($price_varient1))
+      if(!empty($first_varientid))
       {
       $result2 = '
 <div data-tag="'.htmlspecialchars($allproductatgs).'" class="'.htmlspecialchars($allproductatgs).' product-index desktop-3 tablet-2 mobile-half" data-alpha="" data-price="'.$price_varient1.'" style="height:620px;">     
