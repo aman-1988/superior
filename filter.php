5d9396549bffa0a953471735684a132854ec7260
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-30 13:03:41 +0530

    Update filter.php

--- filter.php
+++ filter.php
@@ -288,7 +288,7 @@ $productss = getorder("https://".$SHOPIFY_SHOP."/admin/api/2020-07/products.json
       $admin_graphql_api_id = $product_line_items[$keys3]['admin_graphql_api_id'];
       $allproductatgs = $product_line_items[$keys3]['tags'];
       
-      //$productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");
+      $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");
       // $product_line_items3 = $productss2['product'];  
         // $price_varient1 = $product_line_items3['variants'][0]['price'];
          //$price_compare_at_price = $product_line_items3['variants'][0]['compare_at_price'];
