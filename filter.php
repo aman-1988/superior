1771e281d17b52b18a6812956aa6277b8190d255
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-30 13:33:09 +0530

    Update filter.php

--- filter.php
+++ filter.php
@@ -214,22 +214,25 @@ $duplicates = count($str_arrs_1) - count($unique_colors);
  
  function getproductss($productid2, $protitle2, $proname2, $handle1, $proimgs1, $product_type1, $admin_graphql_api_id, $allproductatgs, $price_varient1, $price_compare_at_price, $first_varientid)
  {
-   $queries3 = array('query' => 'query { product(id: "'.$admin_graphql_api_id.'") { variants(first:1) { edges { node { compareAtPrice  price } } }    } }');
-   $productss2 = httppost("https://".$SHOPIFY_SHOP."/admin/api/unstable/graphql.json",$queries3);
-  
- // print_r($productss2);
-  
+  // $queries3 = array('query' => 'query { product(id: "'.$admin_graphql_api_id.'") { variants(first:1) { edges { node { compareAtPrice  price } } }    } }');
+  // $productss2 = httppost("https://".$SHOPIFY_SHOP."/admin/api/unstable/graphql.json",$queries3);
+
    //$price_varient1 = $productss2['data']['product']['variants']['edges'][0]['node']['price'];
-  // $price_compare_at_price = $productss2['data']['product']['variants']['edges'][0]['node']['compareAtPrice'];   
+  // $price_compare_at_price = $productss2['data']['product']['variants']['edges'][0]['node']['compareAtPrice']; 
   
-  if($price_varient1 < $price_compare_at_price) {
-       $price1 = '<div class="onsale">$'.$price1.'</div><div class="was">$'.$price_compare_at_price.'</div>';
+  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");
+  $product_line_items3 = $productss2['product'];  
+  $price_varient1_1 = $product_line_items3['variants'][0]['price'];
+  $price_compare_at_price_1 = $product_line_items3['variants'][0]['compare_at_price'];
+  
+  if($price_varient1_1 < $price_compare_at_price_1) {
+       $price1 = '<div class="onsale">$'.$price1.'</div><div class="was">$'.$price_compare_at_price_1.'</div>';
       } else {
-       $price1 = '<div class="prod-price"><span class="normal-money">Price $'.$price_varient1.'</span></div>';
+       $price1 = '<div class="prod-price"><span class="normal-money">Price $'.$price_varient1_1.'</span></div>';
       }
       
-      if( $price_varient1 > 1000 && $product_type1 == 'Equipment') {       
-       $price1 = '<span class="Retail">Retail Price</span><span class="money"> $'.$price_varient1.'</span><br><button style="text-color: #FFFFFF;"><span class="email"><a style="color: #FFFFFF;" href="'.$collpath.'/products/'.$handle1.'">Email Me My Price</span></a></button>';
+      if( $price_varient1_1 > 1000 && $product_type1 == 'Equipment') {       
+       $price1 = '<span class="Retail">Retail Price</span><span class="money"> $'.$price_varient1_1.'</span><br><button style="text-color: #FFFFFF;"><span class="email"><a style="color: #FFFFFF;" href="'.$collpath.'/products/'.$handle1.'">Email Me My Price</span></a></button>';
       }
  return $price1;
   
