5f5ee44fdb3e1d12b71ff5ddd3d6fc3e8e4ee224
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-11-04 16:49:16 +0530

    Update condition_for_loop.php

--- condition_for_loop.php
+++ condition_for_loop.php
@@ -1,6 +1,6 @@
 <?php
       if(empty($varientss)) {
-         $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");
+         $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");
       }
        else if(!empty($str_arr)) {
              
@@ -28,51 +28,51 @@
           
              
          if(count($str_arr) >= 6) {
-             if(count($result) >= count($str_arr)-2) {   $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");  }
-            else if($duplicates >= 1 && count($result) >= 4) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json"); }
-            else if($duplicates >= 2 && count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");   }
+             if(count($result) >= count($str_arr)-2) {   $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");  }
+            else if($duplicates >= 1 && count($result) >= 4) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published"); }
+            else if($duplicates >= 2 && count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");   }
          }    
          else if(count($str_arr) == 5) {
             //echo $color_counts1."-";
             //echo $duplicates;
-            if(count($result) >= count($str_arr)-1) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");  }
-            else if($duplicates >= 3) { if(count($result) >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");  } }
-            else if($color_counts1 >= 2  && $duplicates >= 2) { if(count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");  } }
-            else if($duplicates >= 2) { if(count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");  } }  
-            else if($duplicates >= 2 && count($result) >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");   }
-            else if($duplicates >= 1 && count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json"); }   
-           else if($duplicates >= 4) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json"); }
+            if(count($result) >= count($str_arr)-1) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");  }
+            else if($duplicates >= 3) { if(count($result) >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");  } }
+            else if($color_counts1 >= 2  && $duplicates >= 2) { if(count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");  } }
+            else if($duplicates >= 2) { if(count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");  } }  
+            else if($duplicates >= 2 && count($result) >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");   }
+            else if($duplicates >= 1 && count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published"); }   
+           else if($duplicates >= 4) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published"); }
             
          }
          else if(count($str_arr) == 4)
          {
           //echo $duplicates;      
-             if(count($result) == count($str_arr)) {    $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");  } 
+             if(count($result) == count($str_arr)) {    $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");  } 
             // else if($color_counts1 >= 3 && count($result) >= 2) { print_r($result2); }
            //  else if($size_counts1 >= 3 && count($result) >= 2) { print_r($result2); }
             // else if($material_counts1 >= 3 && count($result) >= 2) { print_r($result2); }
             // else if($color_counts1 >= 1 && $material_counts1 >= 1 && count($result) >= 3) { print_r($result2); }  
-            else if($duplicates >= 2 && count($result) >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");   }
-            else if($duplicates >= 1 && count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");   }
-           else if($duplicates >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json"); }
+            else if($duplicates >= 2 && count($result) >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");   }
+            else if($duplicates >= 1 && count($result) >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");   }
+           else if($duplicates >= 3) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published"); }
                   
          } 
          else if(count($str_arr) == 3)
          {
           //echo $duplicates;      
-             if(count($result) == count($str_arr)) {    $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");  } 
+             if(count($result) == count($str_arr)) {    $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");  } 
             // else if($color_counts1 >= 3 && count($result) >= 2) { print_r($result2); }
            //  else if($size_counts1 >= 3 && count($result) >= 2) { print_r($result2); }
             // else if($material_counts1 >= 3 && count($result) >= 2) { print_r($result2); }
             // else if($color_counts1 >= 1 && $material_counts1 >= 1 && count($result) >= 3) {  $productss2 = httppost("https://".$SHOPIFY_SHOP."/admin/api/unstable/graphql.json",$queries3); }         
-             else if($duplicates >= 1 && count($result) >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");   }   
-            else if($duplicates >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");   }  
+             else if($duplicates >= 1 && count($result) >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");   }   
+            else if($duplicates >= 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");   }  
           
          } else if(count($str_arr) == count($result)) {
-              $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");
+              $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");
          } else if(count($result) == 1 && count($str_arr) == 2 && $duplicates >= 1) {
-             $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json"); 
-         } else if(count($result) == 2 && count($str_arr) == 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json"); }
+             $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published"); 
+         } else if(count($result) == 2 && count($str_arr) == 2) {  $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published"); }
           
          
           
@@ -83,7 +83,7 @@
          }
          
          } else {
-            $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json");
+            $productss2 = getorder2("https://".$SHOPIFY_SHOP."/admin/api/unstable/products/".$productid2.".json?published_status=published");
              
          }
 
