552a82410a8a8967703df9a567e4bd818a27534b
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-11-04 19:00:02 +0530

    Update filter.php

--- filter.php
+++ filter.php
@@ -412,6 +412,8 @@ if(strpos($mystring3, $word62) !== false) {
 
      include('condition_for_loop.php');
         $product_line_items3 = $productss2['product'];  
+      
+       $pro_published_at = $product_line_items3['published_at'];
        $price_varient1 = $product_line_items3['variants'][0]['price'];
        $price_compare_at_price = $product_line_items3['variants'][0]['compare_at_price'];
         $first_varientid = $product_line_items3['variants'][0]['id'];
@@ -443,7 +445,7 @@ if(strpos($mystring3, $word62) !== false) {
          
           //$result2 = $handle1. " - $".$price1." - ".$proimgs1;
 // if(!empty($first_varientid) && $var_inventory_policy != 'continue' && $var_inventory_quantity >= '0' || !empty($first_varientid) && $var_inventory_policy == 'continue')
-     if(!empty($first_varientid)) {
+      if(!empty($first_varientid) && !empty($pro_published_at)) {
         
       $result2 = '
 <div data-tag="'.htmlspecialchars($allproductatgs).'" class="'.htmlspecialchars($allproductatgs).' product-index desktop-3 tablet-2 mobile-half" data-alpha="" data-price="'.$price_varient1.'" style="height:620px;">     
