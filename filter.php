8701165cfbcfd8347084b4a7a573884a2644c862
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-11-04 19:09:58 +0530

    Update filter.php

--- filter.php
+++ filter.php
@@ -571,6 +571,7 @@ if(strpos($mystring3, $word62) !== false) {
       unset($var_inventory_policy);
       unset($var_inventory_quantity);
        unset($pro_published_at);
+      unset($result2);
        unset($result);
        $counter++;
      }     
