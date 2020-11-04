873e4cc88fdc2c7fd9e724a7a9905bbcefc67c66
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-11-04 17:30:57 +0530

    Update inc_filter.php

--- inc_filter.php
+++ inc_filter.php
@@ -123,6 +123,7 @@ if(strpos($mystring3, $word61) !== false) {
         $price_varient1 = $product_line_items3['variants'][0]['price'];
         $price_compare_at_price = $product_line_items3['variants'][0]['compare_at_price'];
         $first_varientid = $product_line_items3['variants'][0]['id'];
+        $var_inventory_policy = $product_line_items3['variants'][0]['inventory_policy'];
        //$first_varientid = $product_line_items[$keys3]['variants'][0]['id'];
         
        // $price_varient1 = $product_line_items[$keys3]['variants'][0]['price'];
@@ -140,7 +141,7 @@ if(strpos($mystring3, $word61) !== false) {
       
          
           //$result2 = $handle1. " - $".$price1." - ".$proimgs1;
-   if(!empty($first_varientid))
+   if(!empty($first_varientid) && $var_inventory_policy != 'deny')
    {
       $result2 = '
 <div data-tag="'.htmlspecialchars($product_line_items5[$keys3]['tags']).'" class="'.htmlspecialchars($product_line_items5[$keys3]['tags']).' product-index desktop-3 tablet-2 mobile-half" data-alpha="" data-price="'.$price_varient1.'" style="height:620px;">     
