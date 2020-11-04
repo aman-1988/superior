2a7c65902c418c232df26d05a66d068c28ddb1cc
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-11-04 18:50:37 +0530

    Update inc_filter.php

--- inc_filter.php
+++ inc_filter.php
@@ -142,8 +142,8 @@ if(strpos($mystring3, $word61) !== false) {
       
          
           //$result2 = $handle1. " - $".$price1." - ".$proimgs1;
- if(!empty($first_varientid) && $var_inventory_policy != 'continue' && $var_inventory_quantity >= '0' || !empty($first_varientid) && $var_inventory_policy == 'continue')
-   {
+ //if(!empty($first_varientid) && $var_inventory_policy != 'continue' && $var_inventory_quantity >= '0' || !empty($first_varientid) && $var_inventory_policy == 'continue')
+   if(!empty($first_varientid)) {
      
       $result2 = '
 <div data-tag="'.htmlspecialchars($product_line_items5[$keys3]['tags']).'" class="'.htmlspecialchars($product_line_items5[$keys3]['tags']).' product-index desktop-3 tablet-2 mobile-half" data-alpha="" data-price="'.$price_varient1.'" style="height:620px;">     
